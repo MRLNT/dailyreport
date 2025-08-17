c4d835260c16f65d751551f5fc87f90a66625b7f
MRLNT <marcelantonio246@gmail.com>
2025-08-17 20:03:46 +0700

    add ui web and simple logic

---
+++ src/main/java/com/mrlnt/dailyreport/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.mrlnt.dailyreport.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+    @PostMapping("/submit")
+    public String submitForm(@RequestParam("username") String username, Model model){
+        model.addAttribute("username", username);
+        return "result";
+    }
+}
